c3327e48457a8fa97813e5e28cc2e9e22fec81c0
M. Andhika Dwiki Nugraha <158553151+AndhikaNugRah@users.noreply.github.com>
2024-08-20 22:11:31 +0700

    Add files via upload

---
+++ members/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  homeuniversity = models.CharField(max_length=255, null=True)
+  major = models.CharField(max_length=255,null=True)
+  slug = models.SlugField(default="", null=False)
+  
+  def __str__(self):
+    return f"{self.firstname} {self.lastname}"
